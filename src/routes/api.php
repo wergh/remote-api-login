ff57d1f000ca073dcb5433214879e2e3d52620a9
wergh <carlos.cousillas@gmail.com>
2025-02-02 12:57:27 +0100

    feat(changed-routes): Changed the routes to add the config parameterers

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 use Wergh\RemoteApiLogin\Controllers\ApiGetTokenController;
 use Wergh\RemoteApiLogin\Controllers\ApiRequestLoginController;
 
-Route::get('/login-request', ApiRequestLoginController::class);
-Route::post('/get-token', ApiGetTokenController::class);
+Route::get(Config::get('remote-api-login.request_url'), ApiRequestLoginController::class);
+Route::post(Config::get('remote-api-login.token_url'), ApiGetTokenController::class);
